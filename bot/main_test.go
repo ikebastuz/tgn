ba7bc1c13eef014d29bca588f0b28f3e719365de
Alexandr Kobrin <alexk@canva.com>
2025-04-30 15:22:17 +1000

    save

--- bot/main_test.go
+++ bot/main_test.go
@@ -193,43 +193,48 @@ func TestCreateReply(t *testing.T) {
 		assertNonErrorReply(t, got, want, err)
 	})
 	//
-	// t.Run("WAITING state, - tells about waiting for connection", func(t *testing.T) {
-	// 	store := NewInMemoryStore()
-	// 	store.SetDialogState(&USER_ID, types.DialogState{State: types.STATE_WAITING_FOR_CONNECT})
-	// 	var FROM = types.From{
-	// 		ID:       int64(USER_ID),
-	// 		USERNAME: "hello",
-	// 	}
-	// 	want := []types.ReplyDTO{
-	// 		{
-	// 			UserId: FROM.ID,
-	// 			Messages: []types.ReplyMessage{
-	// 				{
-	// 					Message:     MESSAGE_WAITING_FOR_CONNECTION,
-	// 					ReplyMarkup: nil,
-	// 				},
-	// 			},
-	// 		},
-	// 	}
-	//
-	// 	update := types.TelegramUpdate{
-	// 		UpdateID: 1,
-	// 		Message: types.Message{
-	// 			MessageID: 1,
-	// 			Text:      "",
-	// 			From:      FROM,
-	// 		},
-	// 	}
-	//
-	// 	got, err := createReply(update, store)
-	// 	if err != nil {
-	// 		t.Errorf("shouldn't have error")
-	// 	}
-	//
-	// 	if !reflect.DeepEqual(got, want) {
-	// 		t.Errorf("expected %v, got %v", want, got)
-	// 	}
-	// })
+	t.Run("WAITING state, - tells about waiting for connection", func(t *testing.T) {
+		store := NewInMemoryStore()
+		sm := types.StateMachine{}
+		sm.SetState(&types.WaitingForConnectState{
+			ConnectionId: &CONNECTION_ID,
+		})
+		store.states[*&FROM.ID] = &sm
+
+		var FROM = types.From{
+			ID:       int64(USER_ID),
+			USERNAME: "hello",
+		}
+		want := []types.ReplyDTO{
+			{
+				UserId: FROM.ID,
+				Messages: []types.ReplyMessage{
+					{
+						Message:     MESSAGE_WAITING_FOR_CONNECTION,
+						ReplyMarkup: nil,
+					},
+				},
+			},
+		}
+
+		update := types.TelegramUpdate{
+			UpdateID: 1,
+			Message: types.Message{
+				MessageID: 1,
+				Text:      "",
+				From:      FROM,
+			},
+		}
+
+		got, err := createReply(update, store)
+		if err != nil {
+			t.Errorf("shouldn't have error")
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("expected %v, got %v", want, got)
+		}
+	})
 	//
 	// t.Run("SELECT ROLE state - update both users and ask for lower bounds", func(t *testing.T) {
 	// 	store := NewInMemoryStore()
